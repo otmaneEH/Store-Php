515854558b7a925f2a9decf86582d92bae563bf0
elhaddaji_Otmane <76048466+otmaneelhaddaji@users.noreply.github.com>
2022-06-15 15:13:36 +0100

    Update register.php

--- register.php
+++ register.php
@@ -15,21 +15,21 @@
 		$_SESSION['lastname'] = $lastname;
 		$_SESSION['email'] = $email;
 
-		if(!isset($_SESSION['captcha'])){
-			require('recaptcha/src/autoload.php');		
-			$recaptcha = new \ReCaptcha\ReCaptcha('6LevO1IUAAAAAFCCiOHERRXjh3VrHa5oywciMKcw', new \ReCaptcha\RequestMethod\SocketPost());
-			$resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+		// if(!isset($_SESSION['captcha'])){
+		// 	require('recaptcha/src/autoload.php');		
+		// 	$recaptcha = new \ReCaptcha\ReCaptcha('6LevO1IUAAAAAFCCiOHERRXjh3VrHa5oywciMKcw', new \ReCaptcha\RequestMethod\SocketPost());
+		// 	$resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
 
-			if (!$resp->isSuccess()){
-		  		$_SESSION['error'] = 'Please answer recaptcha correctly';
-		  		header('location: signup.php');	
-		  		exit();	
-		  	}	
-		  	else{
-		  		$_SESSION['captcha'] = time() + (10*60);
-		  	}
+		// 	if (!$resp->isSuccess()){
+		//   		$_SESSION['error'] = 'Please answer recaptcha correctly';
+		//   		header('location: signup.php');	
+		//   		exit();	
+		//   	}	
+		//   	else{
+		//   		$_SESSION['captcha'] = time() + (10*60);
+		//   	}
 
-		}
+		// }
 
 		if($password != $repassword){
 			$_SESSION['error'] = 'Passwords did not match';
@@ -65,72 +65,74 @@
 						<p>Password: ".$_POST['password']."</p>
 						<p>Please click the link below to activate your account.</p>
 						<a href='http://localhost/ecommerce/activate.php?code=".$code."&user=".$userid."'>Activate Account</a>
-					";
+					";}
+					catch(PDOException $e){
+						$_SESSION['error'] = $e->getMessage();
+						header('location: signup.php');
+					}
 
 					//Load phpmailer
-		    		require 'vendor/autoload.php';
+	// 	    		require 'vendor/autoload.php';
 
-		    		$mail = new PHPMailer(true);                             
-				    try {
-				        //Server settings
-				        $mail->isSMTP();                                     
-				        $mail->Host = 'smtp.gmail.com';                      
-				        $mail->SMTPAuth = true;                               
-				        $mail->Username = 'testsourcecodester@gmail.com';     
-				        $mail->Password = 'mysourcepass';                    
-				        $mail->SMTPOptions = array(
-				            'ssl' => array(
-				            'verify_peer' => false,
-				            'verify_peer_name' => false,
-				            'allow_self_signed' => true
-				            )
-				        );                         
-				        $mail->SMTPSecure = 'ssl';                           
-				        $mail->Port = 465;                                   
+	// 	    		$mail = new PHPMailer(true);                             
+	// 			    try {
+	// 			        //Server settings
+	// 			        $mail->isSMTP();                                     
+	// 			        $mail->Host = 'smtp.gmail.com';                      
+	// 			        $mail->SMTPAuth = true;                               
+	// 			        $mail->Username = 'alinniwa7@gmail.com';     
+	// 			        $mail->Password = '143055920';                    
+	// 			        $mail->SMTPOptions = array(
+	// 			            'ssl' => array(
+	// 			            'verify_peer' => false,
+	// 			            'verify_peer_name' => false,
+	// 			            'allow_self_signed' => true
+	// 			            )
+	// 			        );                         
+	// 			        $mail->SMTPSecure = 'ssl';                           
+	// 			        $mail->Port = 465;                                   
 
-				        $mail->setFrom('testsourcecodester@gmail.com');
+	// 			        $mail->setFrom('otman@gmail.com');
 				        
-				        //Recipients
-				        $mail->addAddress($email);              
-				        $mail->addReplyTo('testsourcecodester@gmail.com');
+	// 			        //Recipients
+	// 			        $mail->addAddress($email);              
+	// 			        $mail->addReplyTo('testsourcecodester@gmail.com');
 				       
-				        //Content
-				        $mail->isHTML(true);                                  
-				        $mail->Subject = 'ECommerce Site Sign Up';
-				        $mail->Body    = $message;
+	// 			        //Content
+	// 			        $mail->isHTML(true);                                  
+	// 			        $mail->Subject = 'ECommerce Site Sign Up';
+	// 			        $mail->Body    = $message;
 
-				        $mail->send();
+	// 			        $mail->send();
 
-				        unset($_SESSION['firstname']);
-				        unset($_SESSION['lastname']);
-				        unset($_SESSION['email']);
+	// 			        unset($_SESSION['firstname']);
+	// 			        unset($_SESSION['lastname']);
+	// 			        unset($_SESSION['email']);
 
-				        $_SESSION['success'] = 'Account created. Check your email to activate.';
-				        header('location: signup.php');
+	// 			        $_SESSION['success'] = 'Account created. Check your email to activate.';
+	// 			        header('location: signup.php');
 
-				    } 
-				    catch (Exception $e) {
-				        $_SESSION['error'] = 'Message could not be sent. Mailer Error: '.$mail->ErrorInfo;
-				        header('location: signup.php');
-				    }
+	// 			    } 
+				
 
 
-				}
-				catch(PDOException $e){
-					$_SESSION['error'] = $e->getMessage();
-					header('location: register.php');
-				}
+	// 			}
+	// 			catch(PDOException $e){
+	// 				$_SESSION['error'] = $e->getMessage();
+	// 				header('location: register.php');
+	// 			}
 
-				$pdo->close();
+	// 			$pdo->close();
 
-			}
+	// 		}
 
-		}
+	// 	}
 
-	}
-	else{
-		$_SESSION['error'] = 'Fill up signup form first';
-		header('location: signup.php');
-	}
-
+	// }
+	// else{
+	// 	$_SESSION['error'] = 'Fill up signup form first';
+	// 	header('location: signup.php');
+	// 
+}
+			}}
 ?>
