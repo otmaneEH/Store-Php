9e29ec26163a53c1e4a6fdc06e0a071a0459c3d3
elhaddaji_Otmane <76048466+otmaneelhaddaji@users.noreply.github.com>
2022-06-15 19:45:24 +0100

    Update footer.php

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,5 +2,5 @@
     <div class="pull-right hidden-xs">
       <b>All rights reserved</b>
     </div>
-    <strong>Copyright &copy; 2020 Brought To You By<a href="https://itsourcecode.com/">IT SOURCE CODE</a></strong>
+    <strong>Copyright <?php echo date("Y",time()) ; ?> <a href="http://elhaddaji-otmane.netlify.app/">Otmane</a></strong>
 </footer>
